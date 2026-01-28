8ee38ebf31882ea93c0cfe2bb752046f4afc396c
RonnyJimenez19 <jimenez-arturo@hotmail.com>
2026-01-27 19:53:37 -0600

    Se ha agregado una función extra al archivo web.php

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,4 @@ use App\Http\Controllers\PrincipalController;
 
 Route::get('/hello',HomeController::class);
 Route::get('post/mensaje',[PostController::class,'Mensaje']);
+Route::get('post/about/{param?}/{name?}',[PostController::class, "About"]);
